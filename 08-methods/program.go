dc58e3199159b6feaf142c4aa7df62a78cb149f2
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-03 15:45:40 +0530

    methods for builtin types using type alias

--- 08-methods/program.go
+++ 08-methods/program.go
@@ -5,6 +5,12 @@ import (
 	"methods-demo/models"
 )
 
+type myStr string
+
+func (s myStr) length() int {
+	return len(s)
+}
+
 func main() {
 	grapes := models.NewProduct(200, "Grapes", 120, 50, "Food", 2)
 	//fmt.Println(grapes)
@@ -14,4 +20,11 @@ func main() {
 	grapes.ApplyDiscount(10)
 	//fmt.Println(models.ToString(grapes))
 	fmt.Println(grapes.ToString())
+
+	//var test myStr = "Some dummy string"
+	test := myStr("Some dummy string")
+	fmt.Println(test.length())
+
+	var s string = "Some other dummy string"
+	fmt.Println(myStr(s).length())
 }
