08ffacd95fb072ba3f09470f4be655672b367219
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-04 16:33:58 +0530

    fibonacci using goroutines

---
+++ 11-concurrency/09-demo.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func fibonacci(count int, ch chan int) {
+	x, y := 0, 1
+	for i := 0; i < count; i++ {
+		ch <- x
+		x, y = y, x+y
+	}
+	close(ch)
+}
+
+func main() {
+	ch := make(chan int, 10)
+	go fibonacci(20, ch)
+	for no := range ch {
+		fmt.Println(no)
+	}
+}
