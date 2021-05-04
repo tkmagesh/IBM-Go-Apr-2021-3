5da0d6829e3884142c94c018571acb95486eac9f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-04 15:27:48 +0530

    goroutines - assignment - solution

--- 11-concurrency/05-demo.go
+++ 11-concurrency/05-demo.go
@@ -1,21 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func printHello() {
+func print(msg string, in chan string, out chan string) {
 	for i := 0; i < 5; i++ {
-		fmt.Println("Hello")
-	}
-}
-
-func printWorld() {
-	for i := 0; i < 5; i++ {
-		fmt.Println("World")
+		<-in
+		time.Sleep(1 * time.Second)
+		fmt.Println(msg)
+		out <- "done"
 	}
 }
 
 func main() {
-	printHello()
-	printWorld()
+
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	go print("Hello", ch1, ch2)
+	go print("World", ch2, ch1)
+	ch1 <- "start"
+	fmt.Println("Press ENTER...")
+	var input string
+	fmt.Scanln(&input)
 	fmt.Println("Done")
 }
