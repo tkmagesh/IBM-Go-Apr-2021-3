cd7dea0b6a095c544a85c7d7ff0dc1b4962cdec0
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-04 16:19:55 +0530

    buffered channels

--- 11-concurrency/06-demo.go
+++ 11-concurrency/06-demo.go
@@ -2,18 +2,22 @@ package main
 
 import "fmt"
 
-func sum(nos []int) int {
+func sum(nos []int, resultCh chan int) {
 	result := 0
 	for _, no := range nos {
 		result += no
 	}
-	return result
+	resultCh <- result
 }
 
 func main() {
 	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
-
-	result := sum(data)
-	fmt.Println(result)
+	firstSet := data[:len(data)/2]
+	secondSet := data[len(data)/2:]
+	resultCh := make(chan int)
+	go sum(firstSet, resultCh)
+	go sum(secondSet, resultCh)
+	total := <-resultCh + <-resultCh
+	fmt.Println("Total = ", total)
 
 }
---
+++ 11-concurrency/08-demo.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+//buffered channels
+//using the range and 'close()'
+
+func writeData(ch chan int) {
+	fmt.Println("Before writing 10 into the channel")
+	ch <- 10
+	fmt.Println("After writing 10 into the channel")
+	fmt.Println("Before writing 20 into the channel")
+	ch <- 20
+	fmt.Println("After writing 20 into the channel")
+	fmt.Println("Before writing 30 into the channel")
+	ch <- 30
+	fmt.Println("After writing 30 into the channel")
+	fmt.Println("Before writing 40 into the channel")
+	ch <- 40
+	fmt.Println("After writing 40 into the channel")
+	close(ch)
+}
+
+func main() {
+	fmt.Println("Starting the main function")
+	ch := make(chan int, 2)
+	go writeData(ch)
+	//time.Sleep(2 * time.Second)
+	for no := range ch {
+		fmt.Println("Before reading data from the channel")
+		fmt.Println(no)
+		fmt.Println("After reading data from the channel")
+	}
+
+}
