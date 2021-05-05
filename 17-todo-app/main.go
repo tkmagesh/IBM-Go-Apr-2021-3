0b8493ff28e7c21638f28509845c6a35f19d2061
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-05 17:57:40 +0530

    RESTful services using gin & gorm

---
+++ 17-todo-app/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	config "todo-app/config"
+	models "todo-app/models"
+	routes "todo-app/routes"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	config.DB, err = gorm.Open("mysql", config.DbURL(config.BuildConfig()))
+
+	//to configure the release mode
+	gin.SetMode(gin.ReleaseMode)
+
+	if err != nil {
+		log.Fatalf("Error connecting to database : %s", err.Error())
+	}
+	defer config.DB.Close()
+	config.DB.AutoMigrate(&models.Todo{})
+	r := routes.SetupRouter()
+
+	r.Run()
+}
